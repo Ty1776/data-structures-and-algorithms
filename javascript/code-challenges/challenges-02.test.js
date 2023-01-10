c9315b698a70350d37c0c19c85beb8d885cc3591
Tyler Huntley <tylershuntley@hotmail.com>
2023-01-10 20:56:24 +0000

    on q5

--- javascript/code-challenges/challenges-02.test.js
+++ javascript/code-challenges/challenges-02.test.js
@@ -59,6 +59,8 @@ For example, twoToThe([1,2,3]) returns [2,4,8] because 2 ^ 1 = 2, 2 ^ 2 = 4, and
 
 const forLoopTwoToThe = (arr) => {
   // Solution code here...
+  let newArr = arr.map(int => Math.pow(2, int));
+  return newArr;
 };
 
 /* ------------------------------------------------------------------------------------------------
